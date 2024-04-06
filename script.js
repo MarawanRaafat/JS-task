5028261c6d048c92aa867d5ae92f72e2ea2dc004
marutoo ozmaky <marawanraafat999@gmal.com>
2024-04-07 01:41:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function getInputValue() {
+const value = decument.querySelector("numer-input").value
+const num = decument.querySelector("var")
+num.innerHTML = value / 39.37
+}
+
+decument.querySelector("calculate").onclick = getInputValue
+
+// ايه المشكلة بقا ؟
+// و انا رابط الينك صح 😁
+
